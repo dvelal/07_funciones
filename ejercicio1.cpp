f48c03a40aeaed295603c52c93e550a4d4bff60f
dvelal <dvelal@unjbg.edu.pe>
2025-05-29 22:36:16 -0500

    Agrega ejercicio 1: suma, resta, mult y divi..

---
+++ ejercicio1.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+using namespace std;
+
+void resta(int val1, int val2);
+void cociente(int val1, int val2);
+int producto(int val1, int val2);
+int suma(int val1, int val2);
+
+int main() {
+    float num1, num2, resultado, resultado2;
+    cout<<"ingrese el primer numero: "; cin>>num1;
+    cout<<"ingrese el segundo numero: "; cin>>num2;
+
+    resta(num1,num2);
+    resta(num2,num1);
+
+    resultado = producto(num1,num2);
+
+    cout<<"\nLa multiplicacion de "<<num1<<" por "<<num2<<" es: "<<resultado;
+
+    cociente(num1,num2);
+    cociente(num2,num1);
+
+    resultado2 = suma(num1,num2);
+
+    cout<<"\nLa suma de "<<num1<<" mas "<<num2<<" es: "<<resultado2;
+    return 0;
+}
+
+//funcion resta
+void resta(int val1, int val2){
+    int sustrac; 
+    sustrac = val1-val2;
+    cout<<"\nLa resta de "<<val1<<" - "<<val2<<" es: "<<sustrac;
+}
+
+//funcion producto
+int producto(int val1, int val2){
+    int prod;
+    prod = val1*val2;
+    return(prod);
+}
+
+//funcion suma
+int suma(int val1, int val2){
+    int adici; 
+    adici = val1+val2;
+    return(adici);
+}
+
+//funcion cociente
+void cociente(int val1, int val2){
+    int coci;
+    if(val2!=0){
+        coci = val1/val2;
+        cout<<"\nLa divicion de "<<val1<<" / "<<val2<<" es: "<<coci;
+    }
+    else
+    cout<<"\nLa divicion de "<<val1<<" / "<<val2<<" es: "<<"indeterminado";
+    ;
+}
