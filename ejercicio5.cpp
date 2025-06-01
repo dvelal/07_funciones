161ed6cb0d0d96fa3b65c2c1ed4425c9bf7d1192
dvelal <dvelal@unjbg.edu.pe>
2025-06-01 10:43:33 -0500

    Agrega ejercicio5.cpp: numeros amigos

---
+++ ejercicio5.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int sumaDivisores(int val){
+	int suma = 0;
+	for(int i=1; i<val; i++){
+		if (val%i == 0)
+			suma = suma + i;
+	}
+	return (suma);
+}
+
+int main() {
+	int num1, num2, ax, bx;
+	cout<<"Ingrese los numeros a determinar si son amigos: ";
+	cin>>num1>>num2;
+	ax = sumaDivisores(num1);
+	bx = sumaDivisores(num2);
+	if (ax==num2 && bx==num1)
+		cout<<num1<<" y "<<num2<<" son numeros amigos.";
+	else
+		cout<<num1<<" y "<<num2<<" no son numeros amigos.";
+	return 0;
+}
