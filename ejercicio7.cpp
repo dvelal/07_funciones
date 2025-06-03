616cfefaffceda7dad0a0b37c5557f362b179520
dvelal <dvelal@unjbg.edu.pe>
2025-06-03 09:53:36 -0500

    Agrega ejercicio7.cpp: combinatorio

---
+++ ejercicio7.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+long long factorial(int);
+
+int main() {
+    int m, n;
+    long long factorial_m, factorial_n, factorial_menos_n, combinatorio;
+
+    cout << "Ingrese el valor de m: ";
+    cin >> m;
+    cout << "Ingrese el valor de n: ";
+    cin >> n;
+
+    // Calculos
+    factorial_m = factorial(m);
+    factorial_n = factorial(n);
+    factorial_menos_n = factorial(m - n);
+
+    combinatorio = factorial_m / (factorial_n * factorial_menos_n);
+
+    // Resultado
+    cout << "El valor de C(" << m << ", " << n << ") es: " << combinatorio << endl;
+
+    return 0;
+}
+
+// Funcion para calcular el factorial de un numero
+long long factorial(int val) {
+    long long f = 1;
+    for (int i = 1; i <= val; ++i) {
+        f = f*i;
+    }
+    return f;
+}
